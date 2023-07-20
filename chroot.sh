6e5aab9f0247421b34ff363b596953701b016cac
Quico Augustijn <quico.public@gmail.com>
2023-07-20 12:13:09 +0200

    Move mount binding runtime directories to chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -15,5 +15,16 @@ target=`echo "${config}" | grep --ignore-case 'target' | awk '{print $2}'`
 sudo mkdir --parents "${target}/scripts"
 sudo cp --preserve=mode --recursive ${this}/* "${target}/scripts/"
 
+# Make sure system runtime directories exist
+sudo mkdir --parents ${target}/{dev,proc,run,sys,tmp}
+
+# Bind-mount system runtime directories
+for i in dev dev/pts dev/shm proc run sys tmp; do
+	directory="${target}/$i"
+
+	mount | grep "${directory}" &> /dev/null || \
+		sudo mount --bind /$i --target "${directory}"
+done
+
 # Change system root and execute the setup
 sudo chroot "${target}" "/scripts/setup.sh" $@
--- mount.sh
+++ mount.sh
@@ -16,13 +16,3 @@ directory=`echo "${config}" | grep --ignore-case 'directory' | awk '{print $2}'`
 mount | grep "${directory}" &> /dev/null || \
 	sudo mount --rw --options defaults,noatime \
 		--source ${device} --target ${directory} $@
-
-sudo mkdir --parents ${directory}/{dev,proc,run,sys,tmp}
-
-# Bind-mount system runtime directories
-for i in dev dev/pts dev/shm proc run sys tmp; do
-	target="${directory}/$i"
-
-	mount | grep "${target}" &> /dev/null || \
-		sudo mount --bind /$i --target "${target}"
-done
